060bc485822055dba4e94a1c7bc103a35c0f1384
DarthMatPL <kordianezez@gmail.com>
2024-09-03 19:26:56 +0200

    first_kodland.py
    111

---
+++ kodland.py
@@ -0,0 +1,11 @@
+new_words = {
+    'LOL' : 'odpowiedź na coś zabawnego',
+    'CRINGE' : 'coś dziwnego lub wstydliwego',
+    'ROFL' : 'odpowiedź na żart',
+    'SHEESH' : 'lekka dezaprobata',
+    'CREEPY' : 'straszny, złowieszczy',
+    'AGGRO' : 'stać się agresywnym/zły'
+}
+while True:
+    slowo = input('wyszukaj slowo:')
+    print(new_words[slowo])
